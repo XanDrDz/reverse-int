0b79297ffa8a0a7433de2ee1440fb9a5e2f924d0
Dzyubinskiy Aleksandr <timik_333@mail.ru>
2021-03-02 21:01:29 +0500

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) {n = -n};
+    let a = n.toString();
+    let l = a.split('');
+    let e = l.reverse();
+    let x = e.join('');
+    return x;
 }
